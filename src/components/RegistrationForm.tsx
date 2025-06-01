b90e7c1e4d1bd0f0a5356b994ef9876ce6bb94ab
Nirmalya <nirmalyaganguly1999@gmail.com>
2025-06-01 11:37:17 +0530

    refactor form fields

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -11,12 +11,16 @@ const RegistrationForm = () => {
                 <input type='text' className='form-control' id='username' />
               </div>
               <div className='form-group'>
-                <label htmlFor='username'>Username</label>
-                <input type='text' className='form-control' id='username' />
+                <label htmlFor='email'>Email</label>
+                <input type='email' className='form-control' id='email' />
               </div>
               <div className='form-group'>
-                <label htmlFor='username'>Username</label>
-                <input type='text' className='form-control' id='username' />
+                <label htmlFor='password'>Password</label>
+                <input type='password' className='form-control' id='password' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='confirmPassword'>Confirm Password</label>
+                <input type='password' className='form-control' id='confirmPassword' />
               </div>
               <button type='submit' className='btn btn-primary mt-2'>
                 Submit
